708c3e100833a55df38cd145c32de198aa751144
aidityas <aidityasadhakim250@gmail.com>
2022-10-07 14:21:37 +0700

    Crawling date

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
+from multiprocessing.sharedctypes import Value
+from this import d
+from typing import final
 from unicodedata import name
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -13,10 +18,23 @@ search = driver.find_element(by=By.NAME, value="s")
 
 search.send_keys("text")
 search.send_keys(Keys.RETURN)
+date_list =[]
+try:
+    for i in range(5):    
+        main = WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, "main")))
+        articles = main.find_elements(by=By.TAG_NAME,value="article")
 
-main = driver.find_element(by=By.ID, value="main")
-print(main.text)
+        for article in articles:
+            date = article.find_element(by=By.CLASS_NAME, value="entry-meta")
+            header = date.find_element(by=By.TAG_NAME,value="a")
+            date_list.append(header.text)
+        
+        page_div = main.find_element(by=By.CLASS_NAME, value="paging-navigation")
+        page_button = page_div.find_element(by=By.CLASS_NAME, value="next")
+        page_button.click()
+    
+    print(date_list[0])
+    print(date_list[-1])
 
-time.sleep(10)
-
-driver.quit()
+finally:
+    driver.quit()
